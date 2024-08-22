4e33183ccae190dcd60b745836f2960a61f48036
Dmitry Afanasyev <d.afanasev1612@gmail.com>
2024-08-22 11:50:42 +1000

    Revert "Revert "Revert "изменил на 678"""
    This reverts commit d80db04fc25ce5ab51eeac3a5ab352efa4da293a.

--- blink.ino
+++ blink.ino
@@ -7,7 +7,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 678;
+int led = 250;
 
 // the setup routine runs once when you press reset:
 void setup() {                
