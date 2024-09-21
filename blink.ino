dd12f374e1847d940936b569b92ba30e9651c484
Ismail cisse <ismaelcisse015@gmail.com>
2024-09-21 19:11:49 +0200

    added python version

--- blink.ino
+++ blink.ino
@@ -22,3 +22,24 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
 }
+
+/*
+this a python version of the code, incase if someone who likes python wanted to use it.
+import time
+
+# Simulating pin 13 (LED)
+led = False
+
+# Infinite loop to simulate blinking
+while True:
+    # Turn LED on
+    led = True
+    print("LED is ON")
+    time.sleep(1)  # Wait for 1 second
+
+    # Turn LED off
+    led = False
+    print("LED is OFF")
+    time.sleep(2)  # Wait for 2 seconds
+
+*/
